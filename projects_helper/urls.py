f0c22a4a0ca9324a1502c3bbb66712e7896b0673
mwalercz <mwalerczuk@gmail.com>
2016-06-05 00:46:05 +0200

    added redirect from / to login page

--- projects_helper/urls.py
+++ projects_helper/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -7,6 +8,7 @@ urlpatterns = [
     url(r'^students/', include('projects_helper.apps.students.urls', namespace='students')),
     url(r'^lecturers/', include('projects_helper.apps.lecturers.urls', namespace='lecturers')),
     url(r'^common/', include('projects_helper.apps.common.urls', namespace='common')),
-    #url(r'$', RedirectView.as_view(url='common/login')),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('common:login')), name='redirect_login'),
+    # url(r'$', RedirectView.as_view(url='common/login')),
 
 ]
